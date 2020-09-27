7dc68a9a7f4144b9551f86c7ce4f98a90b34b354
Colin Wheeler <cocoasamurai@gmail.com>
2020-09-26 20:52:25 -0700

    fix unit test so it will compile

--- Tests/SwiftyKeychainTests/SwiftyKeychainTests.swift
+++ Tests/SwiftyKeychainTests/SwiftyKeychainTests.swift
@@ -6,7 +6,6 @@ final class SwiftyKeychainTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftyKeychain().text, "Hello, World!")
     }
 
     static var allTests = [
