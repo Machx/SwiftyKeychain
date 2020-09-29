c1a25043785adc33023c7449d314c6e51d8ace6a
Colin Wheeler <cocoasamurai@gmail.com>
2020-09-28 21:56:14 -0700

    check for the service not set

--- Sources/SwiftyKeychain/Keychain.swift
+++ Sources/SwiftyKeychain/Keychain.swift
@@ -29,6 +29,7 @@ public final class Keychain {
 	}
 	
 	func retrievePassword(withService service: String = "", account: String, accessGroup: String? = nil) -> Result<String,KeychainServiceError> {
+		guard service.count > 0 else { return .failure(.serviceNotSpecified) }
 		var findPasswordQuery = query(withService: service, account: account, accessGroup: accessGroup)
 		configure(&findPasswordQuery, limit: .one, returnAttributes: true, returnData: true)
 		
@@ -58,6 +59,7 @@ public final class Keychain {
 	///   - accessGroup: An optional accessGroup to use to associate with the password.
 	/// - Returns: A result of success (always will return true) if successfully saved, otherwise returns an error.
 	func save(password: String, forAccount account: String, service: String, accessGroup: String? = nil) -> Result<Bool,KeychainServiceError> {
+		guard service.count > 0 else { return .failure(.serviceNotSpecified) }
 		guard let encodedPassword = password.data(using: .utf8) else { return .failure(.errorEncodingData) }
 		
 		let passwordresult = retrievePassword(withService: service, account: account, accessGroup: accessGroup)
