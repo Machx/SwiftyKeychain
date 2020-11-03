b08c1b231c81ac8199e2e88d8312fcf7a2a021ce
Colin Wheeler <cocoasamurai@gmail.com>
2020-11-02 19:59:01 -0800

    test retrieve

--- Tests/SwiftyKeychainTests/SwiftyKeychainTests.swift
+++ Tests/SwiftyKeychainTests/SwiftyKeychainTests.swift
@@ -19,6 +19,33 @@ final class SwiftyKeychainTests: XCTestCase {
 								account: account,
 								accessGroup: nil)
 	}
+	
+	func testVerifyKeychainRetrieve() {
+		let password = "1234"
+		let account = "cdw"
+		let service = "com.SwiftyKeychain.UnitTest"
+		
+		let result = Keychain.save(password: password,
+								   forAccount: account,
+								   service: service)
+		if case let Keychain.KeychainResult.failure(error) = result {
+			XCTFail("Failed with error \(error)")
+			return
+		}
+		
+		let retrieveResult = Keychain.retrievePassword(withService: service,
+													   account: account,
+													   accessGroup: nil)
+		if case let Keychain.KeychainPasswordResult.failure(error) = retrieveResult {
+			XCTFail("Failed with error \(error)")
+		} else if case let Keychain.KeychainPasswordResult.success(retrievedPassword) = retrieveResult {
+			XCTAssertEqual(retrievedPassword, password)
+		}
+		
+		Keychain.removePassword(withService: service,
+								account: account,
+								accessGroup: nil)
+	}
 
     static var allTests = [
         ("testExample", testExample),
